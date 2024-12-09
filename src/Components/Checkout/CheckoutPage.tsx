c2717cae0c7afa07acc95c04aed16a2ce2da2756
Jameel <jameel.bscsf19@iba-suk.edu.pk>
2024-12-09 10:41:22 -0800

    payment checkout redesigned

--- src/Components/Checkout/CheckoutPage.tsx
+++ src/Components/Checkout/CheckoutPage.tsx
@@ -12,26 +12,40 @@ const CheckoutPage = ()=>{
             <form className="form">
                 <h2 className="header">Checkout Payment</h2>
 
-                <TextField label="Name" fullWidth margin="normal" required />
-                <TextField type="email" label="Email" fullWidth margin="normal" required />
-                <TextField label="Address" fullWidth margin="normal" required />
+                <TextField 
+                label="Name"
+                className="inputCheckout" 
+                fullWidth margin="normal" 
+                required 
+                />
+                <TextField 
+                type="email" 
+                label="Email"
+                className="inputCheckout" 
+                fullWidth margin="normal" 
+                required />
+                <TextField 
+                label="Address"
+                className="inputCheckout" 
+                fullWidth margin="normal" 
+                required />
 
                 <FormControl fullWidth margin="normal">
-                <InputLabel>Country of Residence</InputLabel>
-                    <Select defaultValue="" required>
+                <InputLabel >Country of Residence</InputLabel>
+                    <Select defaultValue="" className="inputCheckout" required>
                         {countries.map((country, index) => (
-                        <MenuItem key={index} value={country}>
+                        <MenuItem  key={index} value={country}>
                             {country}
                         </MenuItem>
                         ))}
                     </Select>
                 </FormControl>
 
-                <TextField type="date" fullWidth margin="normal" label="Date of Birth" InputLabelProps={{ shrink: true }} required />
+                <TextField className="inputCheckout" type="date" fullWidth margin="normal" label="Date of Birth" InputLabelProps={{ shrink: true }} required />
 
-                <TextField type="text" label="Card Number" fullWidth margin="normal" required />
-                <TextField type="text" label="CVV" fullWidth margin="normal" required />
-                <TextField type="month" fullWidth margin="normal" label="Expiry Date" InputLabelProps={{ shrink: true }} required />
+                <TextField className="inputCheckout" type="text" label="Card Number" fullWidth margin="normal" required />
+                <TextField className="inputCheckout" type="text" label="CVV" fullWidth margin="normal" required />
+                <TextField className="inputCheckout" type="month" fullWidth margin="normal" label="Expiry Date" InputLabelProps={{ shrink: true }} required />
 
                 <Button variant="contained" type="submit" color="primary" fullWidth>
                 Proceed to Pay
