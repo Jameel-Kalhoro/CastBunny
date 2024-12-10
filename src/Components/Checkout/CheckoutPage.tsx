9e4abb139b88b424c83fbf510ac2f68fc2cafb4c
Jameel <jameel.bscsf19@iba-suk.edu.pk>
2024-12-10 09:53:56 -0800

    payment page redesigned #2

--- src/Components/Checkout/CheckoutPage.tsx
+++ src/Components/Checkout/CheckoutPage.tsx
@@ -7,52 +7,62 @@ const CheckoutPage = ()=>{
     const countries = ["United States", "Canada", "United Kingdom", "Australia", "India"];
 
 
-    return(
-        <div className="containerCheckout">
-            <form className="form">
-                <h2 className="header">Checkout Payment</h2>
-
-                <TextField 
-                label="Name"
-                className="inputCheckout" 
-                fullWidth margin="normal" 
-                required 
-                />
-                <TextField 
-                type="email" 
-                label="Email"
-                className="inputCheckout" 
-                fullWidth margin="normal" 
-                required />
-                <TextField 
-                label="Address"
-                className="inputCheckout" 
-                fullWidth margin="normal" 
-                required />
-
-                <FormControl fullWidth margin="normal">
-                <InputLabel >Country of Residence</InputLabel>
-                    <Select defaultValue="" className="inputCheckout" required>
-                        {countries.map((country, index) => (
-                        <MenuItem  key={index} value={country}>
-                            {country}
-                        </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-
-                <TextField className="inputCheckout" type="date" fullWidth margin="normal" label="Date of Birth" InputLabelProps={{ shrink: true }} required />
-
-                <TextField className="inputCheckout" type="text" label="Card Number" fullWidth margin="normal" required />
-                <TextField className="inputCheckout" type="text" label="CVV" fullWidth margin="normal" required />
-                <TextField className="inputCheckout" type="month" fullWidth margin="normal" label="Expiry Date" InputLabelProps={{ shrink: true }} required />
-
-                <Button variant="contained" type="submit" color="primary" fullWidth>
-                Proceed to Pay
-                </Button>
-            </form>
+    return (
+        <div className="checkout-container">
+          <form  className="checkout-form">
+            <div className="divinbetween">
+            <div className="basic-info">
+              <h3 className="form-h3">Basic Information</h3>
+              <input type="text" placeholder="Name" required />
+              <input type="email" placeholder="Email" required />
+              <input type="text" placeholder="Address" required />
+              <input type="text" placeholder="Country of Residence" required />
+            </div>
+    
+            <div className="payment-details">
+              <h3 className="form-h3">Payment Details</h3>
+              <div className="payment-method">
+                <label>
+                  <input type="radio" name="payment" value="credit-card" defaultChecked />
+                  Credit Card
+                </label>
+                <label>
+                  <input type="radio" name="payment" value="paypal" />
+                  PayPal
+                </label>
+              </div>
+              <input type="text" placeholder="Cardholder Name" required />
+              <input type="text" placeholder="Card Number" required />
+              <div className="card-info">
+                <input type="month" placeholder="Expiry Date" required />
+                <input type="text" placeholder="CVC" required />
+              </div>
+            </div>
+            </div>
+    
+            <div className="summary">
+              <h3>Summary</h3>
+              <div className="summary-item">
+                <span>Professional Plan</span>
+                <span>$39</span>
+              </div>
+              <div className="summary-item">
+                <span>Tax</span>
+                <span>$2.5</span>
+              </div>
+              <div className="summary-item">
+                    <input type="text" placeholder="Apply Coupon" />
+                    <span>Apply</span>
+              </div>
+              <div className="total">
+                <span>Total</span>
+                <span>$41.5</span>
+              </div>
+              <button type="submit">Checkout Now</button>
+            </div>
+          </form>
         </div>
-    );
+      );
 
 }
 
